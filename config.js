931f99e98bb9227dffd7918ee7bceb1eb89e9ba1
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2022-12-04 22:24:10 -0800

    fix

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://powdee:mrRsR8ZAOQZPX6XB@cluster0.3go824g.mongodb.net/merng?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
